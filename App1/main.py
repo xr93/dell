70f7b5d00f8f1711a438d08ba919b8c1b08dce52
Yang Xin Rong <99471368+xr93@users.noreply.github.com>
2025-03-14 09:45:59 +0800

    Update main.py

--- App1/main.py
+++ App1/main.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(page_title="test",page_icon=":shark:",layout="wide")
 st.markdown('#Hello world')
 
 st.sidebar.write("this is main page")
+
+im = Image.open('dell.png')
+st.image(im,width=100)
